1dbf9421bbfac1e7bbb1e381c6292c12e017696b
samuraiotaku <bshimizu@csumb.edu>
2018-10-26 09:23:33 -0700

    Added more Views

---
+++ Cst495-Project/Cst495-Project/DeatailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DeatailViewController.swift
+//  Cst495-Project
+//
+//  Created by Brandon Shimizu on 10/26/18.
+//  Copyright © 2018 rbradley. All rights reserved.
+//
+
+import UIKit
+
+class DeatailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
