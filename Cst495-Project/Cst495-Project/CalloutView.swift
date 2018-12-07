0da1e02c373762299ada1171fec7c044105a6f49
user144731 <user144731@74-80-245-6.ic2net.net>
2018-12-07 02:19:58 -0800

    pushing for like counts

--- Cst495-Project/Cst495-Project/CalloutView.swift
+++ Cst495-Project/Cst495-Project/CalloutView.swift
@@ -11,9 +11,19 @@ import MapViewPlus
 
 class CalloutView: UIView, CalloutViewPlus {
     
+    
     @IBOutlet weak var pinImageView: UIImageView!
     @IBOutlet weak var pinTitle: UILabel!
     @IBOutlet weak var TextBox: UIImageView!
+    @IBOutlet weak var dislikeButton: UIButton!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var likeCount: UILabel!
+    
+    var Liked: Bool?
+    var disLiked: Bool?
+    var alreadyLiked: Bool?
+    var alreadyDisLiked: Bool?
+    var counter: Int = 1
     
     func configureCallout(_ viewModel: CalloutViewModel) {
         let viewModel = viewModel as! CalloutModel
@@ -33,6 +43,72 @@ class CalloutView: UIView, CalloutViewPlus {
         layer.masksToBounds = true
         layer.borderWidth = 2
         layer.borderColor = UIColor(red:255/255, green:223/255, blue:0/255, alpha: 1).cgColor
+    }
+    
+    @IBAction func onLikeTap(_ sender: Any)
+    {
+        print(counter)
+        
+        
+        
+        if(alreadyLiked == true) {
+            likeButton.setImage(UIImage(named:"pinPlusWhite.png"), for: .normal)
+            counter-=1
+            likeCount.text = String(counter)
+            
+            alreadyLiked = false
+        }
+        else{
+            Liked = true
+            disLiked = false
+
+            if (Liked==true)
+            {
+                likeButton.setImage(UIImage(named:"pinPlusLiked35.png"), for: .normal)
+                counter+=1
+                likeCount.text = String(counter)
+                alreadyLiked = true
+                alreadyDisLiked = false
+                dislikeButton.setImage(UIImage(named:"pinMinusWhite.png"), for: .normal)
+            }
+            else{
+                likeButton.setImage(UIImage(named:"pinPlusWhite.png"), for: .normal)
+                counter-=1
+                likeCount.text = String(counter)
+                
+            }
+        }
+    }
+    @IBAction func onDislikeTap(_ sender: Any)
+        {
+        print("dislike Tapped")
+           
+            if(alreadyDisLiked == true)
+            {
+                dislikeButton.setImage(UIImage(named:"pinMinusWhite.png"), for: .normal)
+                counter += 1
+                likeCount.text = String(counter)
+                alreadyDisLiked = false
+            }
+            else{
+                disLiked = true
+                Liked = false
+                if (disLiked == true)
+                {
+                    dislikeButton.setImage(UIImage(named:"pinMinusDisliked.png"), for: .normal)
+                    counter -= 1
+                    likeCount.text = String(counter)
+                    alreadyDisLiked = true
+                    alreadyLiked = false
+                    likeButton.setImage(UIImage(named:"pinPlusWhite.png"), for: .normal)
+                    
+                }
+                else{
+                    likeButton.setImage(UIImage(named:"pinMinusWhite.png"), for: .normal)
+                    counter += 1
+                    likeCount.text = String(counter)
+                }
+            }
     }
     
 }
